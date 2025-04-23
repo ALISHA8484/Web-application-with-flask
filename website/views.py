df88097677c18e48c45b016ed6290428465789b6
ALISHA ._. <alishakerij@gmail.com>
2025-04-23 15:06:04 +0330

    Database file was created.current user was added.The error User undifined is still displayed

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required  # فقط برای کاربران لاگین‌شده
 def home():
-    return render_template("base.html")
+    return render_template("base.html",user=current_user)
+
+@views.route('/test')
+def test():
+    return f"Is Authenticated? {current_user.is_authenticated}"
