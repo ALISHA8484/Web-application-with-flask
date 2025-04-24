222d807aaf5823b2d49cba7ec287b4181f446884
HastiEsm <hastiesmailzade2005@gmail.com>
2025-04-24 20:46:25 +0330

    login , adding and deleting notes , partitioned front end views

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+function deleteNote(noteId)
+{
+    fetch('/delete_note', {
+        method: 'POST',
+    body: JSON.stringify({ noteId: noteId })
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
